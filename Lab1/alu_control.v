db5c8f2d4bab89e1adce2bf6c1e2e9021ff200af
Jeffrey Chen <chenjeffreyh@gmail.com>
2017-10-13 22:42:21 -0700

    * Define lte and zero wire results. * * Removed superfluos 'bit16' mux.

--- Lab1/alu_control.v
+++ Lab1/alu_control.v
@@ -185,7 +185,7 @@ sixteen_bit_alu _16_bit_srl (.a(a >> b),
  *
  * Utilizes the ALU's adder and subtracts b from a by performing a simple add.
  */
-sixteen_bit_alu _16_bit_slt (.a(a),
+sixteen_bit_alu _16_bit_sle (.a(a),
                              .b(b),
                              .b_invert(1),
                              .carry_in(1),
@@ -198,7 +198,7 @@ sixteen_one_mux _bit0 (.sel(ctrl),
                             0,
                             0,
                             0,
-                            result[11][0],
+                            lte,
                             result[10][0],
                             result[9][0],
                             result[8][0],
@@ -217,7 +217,7 @@ sixteen_one_mux _bit1 (.sel(ctrl),
                             0,
                             0,
                             0,
-                            result[11][1],
+                            0,
                             result[10][1],
                             result[9][1],
                             result[8][1],
@@ -236,7 +236,7 @@ sixteen_one_mux _bit2 (.sel(ctrl),
                             0,
                             0,
                             0,
-                            result[11][2],
+                            0,
                             result[10][2],
                             result[9][2],
                             result[8][2],
@@ -255,7 +255,7 @@ sixteen_one_mux _bit3 (.sel(ctrl),
                             0,
                             0,
                             0,
-                            result[11][3],
+                            0,
                             result[10][3],
                             result[9][3],
                             result[8][3],
@@ -274,7 +274,7 @@ sixteen_one_mux _bit4 (.sel(ctrl),
                             0,
                             0,
                             0,
-                            result[11][4],
+                            0,
                             result[10][4],
                             result[9][4],
                             result[8][4],
@@ -293,7 +293,7 @@ sixteen_one_mux _bit5 (.sel(ctrl),
                             0,
                             0,
                             0,
-                            result[11][5],
+                            0,
                             result[10][5],
                             result[9][5],
                             result[8][5],
@@ -312,7 +312,7 @@ sixteen_one_mux _bit6 (.sel(ctrl),
                             0,
                             0,
                             0,
-                            result[11][6],
+                            0,
                             result[10][6],
                             result[9][6],
                             result[8][6],
@@ -331,7 +331,7 @@ sixteen_one_mux _bit7 (.sel(ctrl),
                             0,
                             0,
                             0,
-                            result[11][7],
+                            0,
                             result[10][7],
                             result[9][7],
                             result[8][7],
@@ -350,7 +350,7 @@ sixteen_one_mux _bit8 (.sel(ctrl),
                             0,
                             0,
                             0,
-                            result[11][8],
+                            0,
                             result[10][8],
                             result[9][8],
                             result[8][8],
@@ -369,7 +369,7 @@ sixteen_one_mux _bit9 (.sel(ctrl),
                             0,
                             0,
                             0,
-                            result[11][9],
+                            0,
                             result[10][9],
                             result[9][9],
                             result[8][9],
@@ -388,7 +388,7 @@ sixteen_one_mux _bit10 (.sel(ctrl),
                              0,
                              0,
                              0,
-                             result[11][10],
+                             0,
                              result[10][10],
                              result[9][10],
                              result[8][10],
@@ -407,7 +407,7 @@ sixteen_one_mux _bit11 (.sel(ctrl),
                              0,
                              0,
                              0,
-                             result[11][11],
+                             0,
                              result[10][11],
                              result[9][11],
                              result[8][11],
@@ -426,7 +426,7 @@ sixteen_one_mux _bit12 (.sel(ctrl),
                              0,
                              0,
                              0,
-                             result[11][12],
+                             0,
                              result[10][12],
                              result[9][12],
                              result[8][12],
@@ -445,7 +445,7 @@ sixteen_one_mux _bit13 (.sel(ctrl),
                              0,
                              0,
                              0,
-                             result[11][13],
+                             0,
                              result[10][13],
                              result[9][13],
                              result[8][13],
@@ -464,7 +464,7 @@ sixteen_one_mux _bit14 (.sel(ctrl),
                              0,
                              0,
                              0,
-                             result[11][14],
+                             0,
                              result[10][14],
                              result[9][14],
                              result[8][14],
@@ -483,7 +483,7 @@ sixteen_one_mux _bit15 (.sel(ctrl),
                              0,
                              0,
                              0,
-                             result[11][15],
+                             0,
                              result[10][15],
                              result[9][15],
                              result[8][15],
@@ -497,23 +497,47 @@ sixteen_one_mux _bit15 (.sel(ctrl),
                              result[0][15]}),
                         .out(s[15]));
 
-sixteen_one_mux _bit16 (.sel(ctrl),
-                        .in({0,
-                             0,
-                             0,
-                             0,
-                             result[11][16],
-                             result[10][16],
-                             result[9][16],
-                             result[8][16],
-                             result[7][16],
-                             result[6][16],
-                             result[5][16],
-                             result[4][16],
-                             result[3][16],
-                             result[2][16],
-                             result[1][16],
-                             result[0][16]}),
-                        .out(s[16]));
+/*
+ * A detector for if the ALU result is 0.
+ */
+assign zero = ~(s[0] |
+                s[1] |
+                s[2] |
+                s[3] |
+                s[4] |
+                s[5] |
+                s[6] |
+                s[7] |
+                s[8] |
+                s[9] |
+                s[10] |
+                s[11] |
+                s[11] |
+                s[12] |
+                s[13] |
+                s[14] |
+                s[15]);
+
+/*
+ * 1-bit wire indicating the set on less than or equal to result. The result
+ * should be 1 if either all bits are 0, or the MSb is 1.
+ */
+assign lte = ~(result[11][0] |
+               result[11][1] |
+               result[11][2] |
+               result[11][3] |
+               result[11][4] |
+               result[11][5] |
+               result[11][6] |
+               result[11][7] |
+               result[11][8] |
+               result[11][9] |
+               result[11][10] |
+               result[11][11] |
+               result[11][12] |
+               result[11][13] |
+               result[11][14] |
+               result[11][15]) |
+              result[11][15];
 
 endmodule
