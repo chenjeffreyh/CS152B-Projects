c28f5fc18d2c522c79143977e0cabd86ae7fbe7f
Jeffrey Chen <chenjeffreyh@gmail.com>
2017-11-22 15:37:16 -0800

    * Add all the SDK workspace stuff for Lab 3.

---
+++ Lab3/rock_paper_scissors/Debug/src/helloworld.d
@@ -0,0 +1,36 @@
+src/helloworld.d: ../src/helloworld.c ../src/platform.h \
+ ../src/platform_config.h \
+ ../../rock_paper_scissors_bsp/microblaze_0/include/xgpio.h \
+ ../../rock_paper_scissors_bsp/microblaze_0/include/xil_types.h \
+ ../../rock_paper_scissors_bsp/microblaze_0/include/xil_assert.h \
+ ../../rock_paper_scissors_bsp/microblaze_0/include/xstatus.h \
+ ../../rock_paper_scissors_bsp/microblaze_0/include/xbasic_types.h \
+ ../../rock_paper_scissors_bsp/microblaze_0/include/xgpio_l.h \
+ ../../rock_paper_scissors_bsp/microblaze_0/include/xil_io.h \
+ ../../rock_paper_scissors_bsp/microblaze_0/include/mb_interface.h \
+ ../../rock_paper_scissors_bsp/microblaze_0/include/xil_printf.h \
+ ../../rock_paper_scissors_bsp/microblaze_0/include/xparameters.h
+
+../src/platform.h:
+
+../src/platform_config.h:
+
+../../rock_paper_scissors_bsp/microblaze_0/include/xgpio.h:
+
+../../rock_paper_scissors_bsp/microblaze_0/include/xil_types.h:
+
+../../rock_paper_scissors_bsp/microblaze_0/include/xil_assert.h:
+
+../../rock_paper_scissors_bsp/microblaze_0/include/xstatus.h:
+
+../../rock_paper_scissors_bsp/microblaze_0/include/xbasic_types.h:
+
+../../rock_paper_scissors_bsp/microblaze_0/include/xgpio_l.h:
+
+../../rock_paper_scissors_bsp/microblaze_0/include/xil_io.h:
+
+../../rock_paper_scissors_bsp/microblaze_0/include/mb_interface.h:
+
+../../rock_paper_scissors_bsp/microblaze_0/include/xil_printf.h:
+
+../../rock_paper_scissors_bsp/microblaze_0/include/xparameters.h:
---
+++ Lab3/rock_paper_scissors/Debug/src/platform.d
@@ -0,0 +1,34 @@
+src/platform.d: ../src/platform.c \
+ ../../rock_paper_scissors_bsp/microblaze_0/include/xparameters.h \
+ ../../rock_paper_scissors_bsp/microblaze_0/include/xil_cache.h \
+ ../../rock_paper_scissors_bsp/microblaze_0/include/mb_interface.h \
+ ../../rock_paper_scissors_bsp/microblaze_0/include/xbasic_types.h \
+ ../../rock_paper_scissors_bsp/microblaze_0/include/xil_types.h \
+ ../../rock_paper_scissors_bsp/microblaze_0/include/xparameters.h \
+ ../src/platform_config.h \
+ ../../rock_paper_scissors_bsp/microblaze_0/include/xuartns550_l.h \
+ ../../rock_paper_scissors_bsp/microblaze_0/include/xil_assert.h \
+ ../../rock_paper_scissors_bsp/microblaze_0/include/xil_io.h \
+ ../../rock_paper_scissors_bsp/microblaze_0/include/xil_printf.h
+
+../../rock_paper_scissors_bsp/microblaze_0/include/xparameters.h:
+
+../../rock_paper_scissors_bsp/microblaze_0/include/xil_cache.h:
+
+../../rock_paper_scissors_bsp/microblaze_0/include/mb_interface.h:
+
+../../rock_paper_scissors_bsp/microblaze_0/include/xbasic_types.h:
+
+../../rock_paper_scissors_bsp/microblaze_0/include/xil_types.h:
+
+../../rock_paper_scissors_bsp/microblaze_0/include/xparameters.h:
+
+../src/platform_config.h:
+
+../../rock_paper_scissors_bsp/microblaze_0/include/xuartns550_l.h:
+
+../../rock_paper_scissors_bsp/microblaze_0/include/xil_assert.h:
+
+../../rock_paper_scissors_bsp/microblaze_0/include/xil_io.h:
+
+../../rock_paper_scissors_bsp/microblaze_0/include/xil_printf.h:
